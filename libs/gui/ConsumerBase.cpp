67eeff9dd835c817cf5732902227d0d80968d38a
Patrick Graber <pjgraber03@gmail.com>
2015-06-02 07:43:40 -0700

    Revert "Add ConsumerBase::isAbandoned"
    This reverts commit 2492b27efe0e2c71ea65b4155e81d26c0dd19a06.
    
    Change-Id: I9eb4ed4ad7b08f1b1ca31e2587aa40847dbcb3c1

--- libs/gui/ConsumerBase.cpp
+++ libs/gui/ConsumerBase.cpp
@@ -155,11 +155,6 @@ void ConsumerBase::abandonLocked() {
     mConsumer.clear();
 }
 
-bool ConsumerBase::isAbandoned() {
-    Mutex::Autolock _l(mMutex);
-    return mAbandoned;
-}
-
 void ConsumerBase::setFrameAvailableListener(
         const wp<FrameAvailableListener>& listener) {
     CB_LOGV("setFrameAvailableListener");
